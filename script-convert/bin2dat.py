39a142fb1cc3644ddad2d1c1c24c57c69654cd99
Ariel Burman <arielburman@gmail.com>
2016-01-20 14:45:00 -0300

    Added new probes and an update on the script

--- script-convert/bin2dat.py
+++ script-convert/bin2dat.py
@@ -8,23 +8,18 @@ import time
 
 input_file_patt = 'prueba'
 output_file = 'output.dat'
-canalesT = 25
-#frate = 30030
-#block_length = 27
 
-
-def convert(input_file_patt, output_file,tetrode,filesN=None,appendStrobe=False,channels=[]):
+def convert(input_file_patt, output_file,tetrode,filesN=None,appendStrobe=False,channels=[],offset=0,totCh=25):
 
   if channels == []:
-    canalesElegidos = []
     for i in tetrode:
       for n in range((i-1)*4,i*4):
-        channels.append(n)
+        channels.append(n+offset)
 
     if appendStrobe == True:
-      channels.append(24)
+      channels.append(totCh-1)
 
-  files=glob.glob(input_file_patt+'_*')
+  files=glob.glob(input_file_patt+'*')
   n = len(files)
   if n==0:
     assert 'Ruta inexistente'
@@ -39,19 +34,16 @@ def convert(input_file_patt, output_file,tetrode,filesN=None,appendStrobe=False,
   fout = open(output_file,'wb')
   data = None
   for i in range(fstart,fend+1):
-    fin = input_file_patt+'_'+str(i)
+    fin = input_file_patt+str(i)
     print ('File:', i-fstart+1,'/', fend-fstart+1, '->', fin)
-    newdata=np.fromfile(fin,dtype='int16',count=-1) #sep=''
-    #newdata = newdata.reshape(-1,canalesT)
-    #newdata = newdata[:,canalesElegidos]
-    #newdata.tofile(fout)
-    newdata.reshape(-1,canalesT)[:,channels].tofile(fout)
+    newdata=np.fromfile(fin,dtype='int16',count=-1)
+    newdata.reshape(-1,totCh)[:,channels].tofile(fout)
 
   fout.close()
 
 def show_help():
     print ('  python bin2dat.py -i <inputpattern> -o <outputfile> [-F X,Z][-T 1,...,M] [-S True|False] [-C 0,...,N-1] -h')
-    print ('   -i    Patron de archivos de entrada  (nombre de archivo sin incluir el "_")')
+    print ('   -i    Patron de archivos de entrada  (nombre de archivo incluyendo el "_" o "-" final antes del ultimo numero)')
     print ('   -o    Nombre de archivo de salida')
     print ('   -F    opcional, debe indicarse el numero inicial (X) y final (Z) de archivos a procesar (incluye a Z).')
     print ('         Si no se indica se utilizan todos los archivos que tengan el mismo patron')
@@ -60,14 +52,19 @@ def show_help():
     print ('   -S    opcional, indica si se agrega el canal 25')
     print ('   -C    opcional, debe indicarse una cadea de numeros de 0 a 23 (o 24, donde 24 es strobe) que indica los canales')
     print ('         que se van a utilizar. Al usar esta opcion se ignora la opcion -T y -S')
+    print ('   -Q    opcional, indica si hay que sumar un offset a cada canal (default 0)')
+    print ('   -N    opcional, total de canales (default 25) que tiene el registro independiente de cuantos se utilicen (sirve')
+    print ('         para identificar cada los registros correspondientes a cada instante de tiempo')
     print ('   -h    muestra esta ayuda')
     print (' ')
     print ('  ejemplos:')
-    print ('    python bin2dat.py -i ../../registro/030315/prueba12LC -o salida.dat')
-    print ('    python bin2dat.py -i ../../registro/030315/prueba12LC -o salida.dat -F 0,49 -T 1,2,3')
-    print ('    python bin2dat.py -i ../../registro/030315/prueba12LC -o salida.dat -F 15,23 -T 1,2,3 -S ')
-    print ('    python bin2dat.py -i ../../registro/030315/prueba12LC -o salida.dat -F 0,49 -C 0,2,5,18')
-
+    print ('    python bin2dat.py -i ../../registro/030315/prueba12LC_ -o salida.dat')
+    print ('    python bin2dat.py -i prueba12LC_ -o salida.dat -F 0,49 -T 1,2,3')
+    print ('    python bin2dat.py -i prueba12LC_ -o salida.dat -F 15,23 -T 1,2,3 -S ')
+    print ('    python bin2dat.py -i prueba12LC_ -o salida.dat -F 0,49 -C 0,2,5,18')
+    print ('    python bin2dat.py -i 20160101-1- -o salida.dat -F 1,5 -C 2,4,5,18 -Q 3 -N 40')
+    print (' ')
+    print ('  orden de electrodos I1: 1 2 11 16 10 6 14 15 9 4 7 13 3 5 8 12 17 18 25 20 28 32 23 24 29 26 30 31 19 21 22 27')
 
 def main(argv):
   inputpatt = ''
@@ -76,8 +73,10 @@ def main(argv):
   strobe = False      # si no se especifica, no se agrega
   filesN = None
   channels = []
+  totalChannels = 25
+  offset = 0
   try:
-    opts, args = getopt.getopt(argv,"hi:o:F:T:S:C:",["ifile=","ofile=","filesN=","tetrode=","strobe","channels="])
+    opts, args = getopt.getopt(argv,"hi:o:F:T:S:C:Q:N:",["ifile=","ofile=","filesN=","tetrode=","strobe","channels=","offset=","totalChannels="])
   except getopt.GetoptError:
     print ('Number of arguments:', len(sys.argv), 'arguments.')
     print ('Argument List:', str(sys.argv))
@@ -102,13 +101,19 @@ def main(argv):
       strobe = True
     elif opt in ("-C", "--channels"):
       channels = [int(i) for i in arg.split(',')]
-  print ('Input pattern is :', inputpatt+'_*')
+    elif opt in ("-Q", "--offset"):
+      offset = arg
+    elif opt in ("-N", "--totalChannels"):
+      totalChannels = int(arg)
+  print ('Input pattern is :', inputpatt+'*')
   print ('File num filter  :', filesN)
   print ('Output file is   :', outputfile)
   print ('Tetrodes         :', tetrode)
   print ('Channels         :', channels)
+  print ('TotalChannels    :', totalChannels)
   print ('Add Strobe       :', strobe)
-  convert(inputpatt,outputfile,tetrode,filesN,strobe,channels)
+  print ('Offset           :', offset)
+  convert(inputpatt,outputfile,tetrode,filesN,strobe,channels,offset,totalChannels)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
